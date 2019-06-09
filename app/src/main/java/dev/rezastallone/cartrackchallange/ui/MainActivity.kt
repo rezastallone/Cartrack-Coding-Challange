e66ded07297b53244fe9fb771a8367054fdc17c4
Reza Stallone <rezastallone@gmail.com>
2019-06-09 10:19:56 +0700

    Update signin fragment to be scrollable

--- app/src/main/java/dev/rezastallone/cartrackchallange/ui/MainActivity.kt
+++ app/src/main/java/dev/rezastallone/cartrackchallange/ui/MainActivity.kt
@@ -1,17 +1,18 @@
 package dev.rezastallone.cartrackchallange.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import dev.rezastallone.cartrackchallange.R
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +22,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupToolbarWithNavigation() {
         val navController : NavController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.auth_nav_graph,
+                R.id.main_nav_graph
+            )
+        )
         findViewById<Toolbar>(R.id.action_bar)
             .setupWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
